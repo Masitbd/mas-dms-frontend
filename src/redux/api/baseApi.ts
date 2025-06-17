1d831c4985d7f367db432c5b2ed68816d9030ce3
Maruf-9270 <marufahmed9270@gmail.com>
2025-06-17 11:01:17 +0600

    Initialization of the project

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,12 @@
+// Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
+import config from "@/config";
+import { axiosBaseQuery } from "@/shared/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: axiosBaseQuery({ baseUrl: config.server_url as string }),
+  endpoints: () => ({}),
+  tagTypes: [],
+});
