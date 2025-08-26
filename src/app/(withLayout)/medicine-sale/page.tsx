f0b5584527fa9b502f9c8879c041a5550b47a3f5
Abdullah Al Jubair <jubair2810@gmail.com>
2025-08-26 22:10:20 +0600

    enable-medicine-sales-search

--- src/app/(withLayout)/medicine-sale/page.tsx
+++ src/app/(withLayout)/medicine-sale/page.tsx
@@ -2,23 +2,39 @@
 
 import MedicineSalesTable from "@/components/medicineSales/Sales";
 import { useGetMedicinesSalesQuery } from "@/redux/api/medicines/sales.api";
+import { SearchIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
-import { Button } from "rsuite";
-
+import { Button, Input, InputGroup } from "rsuite";
+import { useDebouncedCallback } from "use-debounce";
 const MedicineSales = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const handleDebounce = useDebouncedCallback((value: string) => {
+    setSearchTerm(value);
+  }, 500);
 
   const queryParams: Record<string, any> = {};
   if (searchTerm) queryParams.searchTerm = searchTerm;
 
   const { data: salesData, isLoading } = useGetMedicinesSalesQuery(queryParams);
 
-
   return (
     <div>
-      <div className="flex justify-between items-center bg-white h-20 w-full px-5 rounded-xl">
+      <div className="flex justify-between items-center bg-white shadow-xl h-20 w-full px-5 rounded-xl">
         <h1 className="text-2xl font-bold">Medicine Sales</h1>
+
+        <InputGroup style={{ width: 500, marginBottom: 10 }}>
+          <Input
+            onChange={(value: string) => {
+              handleDebounce(value);
+            }}
+            placeholder="Search by name, contact no, invoice no"
+          />
+          <InputGroup.Button>
+            <SearchIcon />
+          </InputGroup.Button>
+        </InputGroup>
 
         <Link href="/medicine-sale/create">
           <Button appearance="primary" color="blue" size="lg">
